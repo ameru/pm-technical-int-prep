efef1c01db95cd9c1daa79c7e9ceb2072269c3ed
Amy Ru <amru@calpoly.edu>
2020-09-18 09:50:09 -0700

    Update 2D_array_patterns.py

--- CodePath Sample Questions/2D_array_patterns.py
+++ CodePath Sample Questions/2D_array_patterns.py
@@ -73,3 +73,30 @@ grid[box_center_row-1][box_center_column+1] = 1 #up, right
 grid[box_center_row-1][box_center_column-1] = 1 #up, left
 
 print_grid(grid)
+
+
+# Python3 program to rotate an array by 
+# d elements 
+# Function to left rotate arr[] of size n by d*/ 
+def leftRotate(arr, d, n): 
+	for i in range(d): 
+		leftRotatebyOne(arr, n) 
+
+# Function to left Rotate arr[] of size n by 1*/ 
+def leftRotatebyOne(arr, n): 
+	temp = arr[0] 
+	for i in range(n-1): 
+		arr[i] = arr[i + 1] 
+	arr[n-1] = temp 
+		
+
+# utility function to print an array */ 
+def printArray(arr, size): 
+	for i in range(size): 
+		print ("% d"% arr[i], end =" ") 
+
+
+# Driver program to test above functions */ 
+arr = [1, 2, 3, 4, 5, 6, 7] 
+leftRotate(arr, 2, 7) 
+printArray(arr, 7) 
